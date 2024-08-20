a0f81ad0cad567190bc2b3f14684883f5833e45d
compendious <yasincompendious@gmail.com>
2024-08-20 08:17:14 +0700

    Updated the button interaction

--- index.js
+++ index.js
@@ -3,3 +3,12 @@
 1 liter = 0.264 gallon
 1 kilogram = 2.204 pound
 */
+
+let defaultValue = document.getElementById("default-value")
+let resultValue = document.getElementById("result-value")
+let inputField = document.getElementById("input-field")
+
+defaultValue.textContent = inputField
+
+console.log(inputField)
+
